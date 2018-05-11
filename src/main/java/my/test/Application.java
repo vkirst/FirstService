f0af5bfefd6e2c116770a882c929e9d2f580671c
Victor Kirst <vkirst@ggasoftware.com>
2018-05-11 13:02:52 +0300

    added RabbitMQ using

--- src/main/java/my/test/Application.java
+++ src/main/java/my/test/Application.java
@@ -1,13 +1,13 @@
 package my.test;
 
 import java.io.IOException;
-import java.net.SocketException;
 import java.util.Scanner;
 
 import javax.jms.ConnectionFactory;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jms.JmsComponent;
@@ -18,8 +18,7 @@ public class Application {
     public static void main(String[] args) throws Exception {
 
         CamelContext ctx = new DefaultCamelContext();
-        ConnectionFactory connectionFactory =
-                new ActiveMQConnectionFactory("vm://localhost");
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
         ctx.addComponent("jms",
                 JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
@@ -27,20 +26,15 @@ public class Application {
 
             @Override
             public void configure() throws Exception {
-                onException(IOException.class, SocketException.class)
-                        .maximumRedeliveries(-1).redeliveryDelay(2000);
-
-                restConfiguration().producerComponent("http4")
-                    .host("localhost").port(8080);
+                onException(IOException.class)
+                        .maximumRedeliveries(-1).redeliveryDelay(2000).maximumRedeliveryDelay(2000);
 
                 from("direct:in")
-                    .to("jms:queue:incomingOrders");
+                        .to("jms:queue:incomingOrders");
 
                 from("jms:queue:incomingOrders?acknowledgementModeName=CLIENT_ACKNOWLEDGE")
-                    .setHeader("name", simple("${in.body}"))
-                    .to("rest:get:greeting?name={name}")
-                    .log("${body}");
-
+                        .inOut("rabbitmq:localhost:5672/incomingOrders?username=guest&password=guest&automaticRecoveryEnabled=true")
+                        .log("${body}");
 
             }
         });
@@ -64,8 +58,13 @@ public class Application {
         ctx.stop();
     }
 
-    public void print(String st) {
-        System.err.println(st);
+    public static com.rabbitmq.client.ConnectionFactory rabbitConnectionFactory() {
+        com.rabbitmq.client.ConnectionFactory cf = new com.rabbitmq.client.ConnectionFactory();
+        cf.setHost("localhost");
+        cf.setPort(5672);
+        cf.setUsername("guest");
+        cf.setPassword("guest");
+        return cf;
     }
 
 }
